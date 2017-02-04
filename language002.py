8bfd9bcfc552d6b626f8cdb430a05f85e5bc530d
hiratsukashu <hiratsukashu@indival.co.jp>
2017-02-04 10:01:06 +0900

    language002

---
+++ language002.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+str1 = u"パタトクカシーー"
+aryStr = list(str1) 
+strNew = aryStr[0]+aryStr[2]+aryStr[4]+aryStr[6]
+print(strNew)
+print(aryStr)
---
+++ language002_1.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+str1 = u"パタトクカシーー"
+print(str1[0::2])
