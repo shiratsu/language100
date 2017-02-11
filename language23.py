81d92b222e148e91340139691725484b199ed1ef
hiratsukashu <hiratsukashu@indival.co.jp>
2017-02-11 17:04:31 +0900

    language23

---
+++ language23.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 23.py
+
+import re
+from mymodule import extract_from_json
+
+strCheck = r'^(=+)(\s?).+(=+)(\s?)$'
+
+lines = extract_from_json(u"イギリス").split("\n")
+for line in lines:
+    # print(line)
+    # 連続した小文字のアルファベットを検索する
+    matchObj = re.search(strCheck, line)
+    if matchObj:
+        print(matchObj.group(0))
+        if len(matchObj.group(1)) == 2:
+            print(matchObj.group(0),1)
+        else:
+            print(matchObj.group(0),2)
