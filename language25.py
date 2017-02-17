c85df507040a1028f768a5d9782b9991eaee59d2
hiratsukashu <hiratsukashu@indival.co.jp>
2017-02-17 14:58:30 +0900

    25が終わった。眠い

--- language25.py
+++ language25.py
@@ -6,10 +6,30 @@ import re
 from mymodule import extract_from_json
 
 strCheck = r'\{\{基礎情報.+\}\}$'
+strKiso = r'[^\|]+\s?=\s?.+'
+lines = extract_from_json(u"イギリス").split("\n")
+kisoFlag = False
+dict = {}
 
-lines = re.split(r"\n[\|}]", extract_from_json(u"イギリス"))
 for line in lines:
-    # print(line)
-    # 連続した小文字のアルファベットを検索する
-    print(line)
-    
+
+    if line == '{{基礎情報 国':
+        print("true")
+        kisoFlag = True
+        continue
+    elif kisoFlag == True and line != '}}':
+        # 連続した小文字のアルファベットを検索する
+        # print(line)
+        matchObj = re.search(strKiso, line)
+        if matchObj:
+            tmp = matchObj.group()
+            # print(tmp)
+            aryKiso = tmp.split("=")
+            # print(aryKiso)
+            dict.update({aryKiso[0].strip(): aryKiso[1].strip()})
+    elif line == '}}':
+        print("false")
+        kisoFlag = False
+        continue
+
+print(dict)
