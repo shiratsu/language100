f02e233976d219609c0d229a81ec2fea9e82b6e0
SHUNSUKE HIRATSUKA <shunsukehiratsuka@SHUNSUKEs-MacBook-Pro.local>
2017-03-23 01:02:05 +0900

    31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット31と32を追加でコミット

---
+++ language32.py
@@ -0,0 +1,23 @@
+# coding: UTF-8
+
+import sys # モジュール属性 argv を取得するため
+import MeCab
+import re
+import pprint
+
+# めかぶと辞書配列の定義
+mecab = MeCab.Tagger('mecabrc')
+
+aryInfo = []
+
+i = 0
+for line in open('neko.txt', 'r'):
+
+    analyzetext = mecab.parse(line)
+    # print(analyzetext)
+    aryNode = re.split("[\t,]",analyzetext)
+    # print(aryNode)
+    # print(aryNode[0])
+    if len(aryNode) > 2:
+        if aryNode[1] == '動詞':
+            print(aryNode[7])
